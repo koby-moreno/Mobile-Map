5d750f8464dd6c8ea22d4c69c396af4e4bc567c7
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2024-08-05 19:59:23 -0400

    new

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ let sliderVal = 5;
 let controlBtn = document.getElementById("control-panel-btn");
 let controlPanel = document.getElementById("control-panel");
 let panelState = false;
+let opacityScaleCont = document.getElementById("opacity-scale-cont");
+let densityChartCont = document.getElementById("density-chart-cont");
 
 let maxLandArea = -Infinity;
+
+let landAreas = [];
+let buckets = [];
 
 let sliders = document.querySelectorAll('.slider');
 
@@ -37,18 +42,54 @@ function loadParcelData() {
     return fetch('./extentSmall.json')
         .then(response => response.json())
         .then(parcelData => {
+
             // Loop through each feature in the JSON data
             parcelData.features.forEach((data) => {
                 // Convert the Parcles_CSV_LandArea property to a number
                 const landArea = parseFloat(data.properties.Parcles_CSV_LandArea);
+                if (landArea > 0 && data.properties.Parcles_CS === "TRUE"){
+                landAreas.push(landArea);
+                }
 
                 // Check if the current land area is greater than the maxLandArea
-                if (landArea > maxLandArea) {
-                    maxLandArea = landArea;
-                }
+                // if (landArea > maxLandArea) {
+                //     maxLandArea = landArea;
+                // }
+
             });
 
-            let opacityScale = d3.scaleLinear([0, maxLandArea], [(25 * sliderVal), 255]);
+            maxLandArea = d3.max(landAreas);
+            let opacityScale = d3.scalePow([0, maxLandArea], [(25.5 * sliderVal), 255]).exponent(0.75);
+            let histogram_scale = d3.scaleSqrt([0, maxLandArea], [0, 16]);
+      
+
+            for (let b = 0; b <= 16; b++){
+                buckets.push([]);
+            }
+            
+            console.log(buckets);
+
+            landAreas.forEach((data) => {
+                // if (data > maxLandArea/10){
+                //     data = maxLandArea/10;
+                // }
+                let bucketVal = Math.round(histogram_scale(data));
+                buckets.forEach((bucket, i) => {
+                    if (bucketVal == i){
+                        bucket.push(bucketVal)
+                    }
+                });
+            });
+
+            buckets.forEach((bucket, i) => {
+                const bar = document.createElement('template');
+                let count = bucket.length;
+                let yScale = d3.scaleSqrt([0, 106], [0, 24]);
+                let num = yScale(count);
+                console.log(num);
+                bar.innerHTML = `<div class="grow bg-gray-400" style="height: ${num}px;" id="bar-${i}"></div>`;
+                densityChartCont.appendChild(bar.content);
+            });
 
             // Output the highest value of Parcles_CSV_LandArea
             console.log('Highest Parcles_CSV_LandArea:', maxLandArea);
@@ -56,10 +97,13 @@ function loadParcelData() {
             function fillColor(d) {
                 let opacity = 0;
 
-                if (d.properties.Parcles_CS === "TRUE") {
+                if (d.properties.Parcles_CS === "TRUE" && d.properties.Parcles_CSV_LandArea > 0) {
                     //opacity = 255;
 
                     opacity = opacityScale(d.properties.Parcles_CSV_LandArea);
+                }
+                else if (d.properties.Parcles_CS === "TRUE") {
+                    opacity = 12.5;
                 }
 
                 return [0, 120, 62, opacity];
@@ -94,7 +138,7 @@ function loadParcelData() {
 
             fillSlider.addEventListener("input", function () {
                 sliderVal = fillSlider.value;
-                opacityScale = d3.scaleLinear([0, maxLandArea], [(25 * sliderVal), 255]);
+                opacityScale = d3.scalePow([0, maxLandArea], [(25.5 * sliderVal), 255]).exponent(0.75);
 
                 // Update the layer with the new fill color
                 deckOverlay.setProps({
